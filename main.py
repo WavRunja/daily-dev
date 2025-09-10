a72d2a4d53417b5b6e457422450dc245d8caa53c
WavRunja <pereverzew-wladimir@yandex.ru>
2025-09-10 23:01:13 +0300

    HTTP request by path /items/{item_id}

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 # main.py
+from typing import Union
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message1": "Netbook1 Codium FastAPI 🚀", "message2": "Netbook2 Codium FastAPI 🚀"}
+    return {
+        "Hello": "World",
+        "message1": "Netbook1 Codium FastAPI 🚀",
+        "message2": "Netbook2 Codium FastAPI 🚀"
+        }
+
+
+# HTTP request by path /items/{item_id}
+# Request example:
+# http://127.0.0.1:8000/items/42?q=The_Ultimate_Question_of_Life,_the_Universe,_and_Everything.
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
