d08dafa9f6eda80faad5ff56fb1dbc1ff94b235d
WavRunja <pereverzew-wladimir@yandex.ru>
2025-09-11 11:12:36 +0300

    Declaring the body of a PUT request using standard Python typing.

--- main.py
+++ main.py
@@ -2,8 +2,19 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    product_description: Union[str, None] = None
+    review: Union[str, None] = None
+    is_offer: Union[bool, None] = None
+
 
 @app.get("/")
 def root():
@@ -21,3 +32,11 @@ def root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name,
+    "item_id": item_id,
+    "item_prod_descr": item.product_description,
+    "review": item.review
+    }
