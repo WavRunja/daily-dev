30e64e4f088afb124a2edfe914ccad56d5ab8a2a
WavRunja <pereverzew-wladimir@yandex.ru>
2025-09-12 17:43:13 +0300

    Python Type Annotation

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name,
     "item_id": item_id,
+    "price": item.price,
     "item_prod_descr": item.product_description,
     "review": item.review
     }
